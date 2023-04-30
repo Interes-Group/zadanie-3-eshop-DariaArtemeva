1b1834593a2ab1ed32ff88403d6870708986aa84
DariaArtemeva <xartemeva@gmail.com>
2023-04-30 15:16:00 +0200

    product + test

--- src/main/java/sk/stuba/fei/uim/oop/assignment3/product/Controller.java
+++ src/main/java/sk/stuba/fei/uim/oop/assignment3/product/Controller.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/products")
+@RequestMapping("/product")
 public class Controller {
     @Autowired
     private ProductService productService;
